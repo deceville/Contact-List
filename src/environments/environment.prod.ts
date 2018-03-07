034da1e593f432c77472aeb174ce84c11388cead
deceville <iamdecebel@gmail.com>
2018-03-07 10:52:36 +0800

    Pasted firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9LBMJRD8lm0tJuHb9ilFcJnWZTNnF2i8",
+    authDomain: "contact-list-fe1da.firebaseapp.com",
+    databaseURL: "https://contact-list-fe1da.firebaseio.com",
+    projectId: "contact-list-fe1da",
+    storageBucket: "contact-list-fe1da.appspot.com",
+    messagingSenderId: "750636619767"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9LBMJRD8lm0tJuHb9ilFcJnWZTNnF2i8",
+    authDomain: "contact-list-fe1da.firebaseapp.com",
+    databaseURL: "https://contact-list-fe1da.firebaseio.com",
+    projectId: "contact-list-fe1da",
+    storageBucket: "contact-list-fe1da.appspot.com",
+    messagingSenderId: "750636619767"
+  }
 };
