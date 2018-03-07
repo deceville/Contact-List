6afca335bd37817bc420561b8969c42389e4d9f1
deceville <iamdecebel@gmail.com>
2018-03-08 00:02:44 +0800

    integrated firestore to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+var firebaseConfig: {
+  apiKey: "AIzaSyC9LBMJRD8lm0tJuHb9ilFcJnWZTNnF2i8",
+  authDomain: "contact-list-fe1da.firebaseapp.com",
+  databaseURL: "https://contact-list-fe1da.firebaseio.com",
+  projectId: "contact-list-fe1da",
+  storageBucket: "contact-list-fe1da.appspot.com",
+  messagingSenderId: "750636619767"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
