c9ee63e0a525c4cc590c1d4eeba6130369989490
deceville <iamdecebel@gmail.com>
2018-03-08 00:28:34 +0800

    adding document

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,10 +26,25 @@ export class AppComponent {
   contactsCol: AngularFirestoreCollection<Contact>;
   contacts: Observable<Contact[]>;
 
+  firstName: string;
+  secondName: string;
+  phoneNum: string;
+  mobileNum: string;
+  email: string;
+  homeAddress: string;
+  streetName: string;
+  city: string;
+  province: string;
+  country: string;
+
   constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
     this.contactsCol = this.afs.collection('contacts');
     this.contacts = this.contactsCol.valueChanges();
+  }
+
+  addContact(){
+    this.afs.collection('contacts').add({'firstName': this.firstName, 'secondName': this.secondName, 'phoneNum': this.phoneNum, 'mobileNum': this.mobileNum, 'email': this.email, 'homeAddress': this.homeAddress, 'streetName': this.streetName, 'city': this.city, 'province': this.province, 'country': this.country, });
   }
 }
