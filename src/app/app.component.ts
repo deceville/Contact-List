9b18d3ea0255ec5a8d38a3fe903b36e8e377c580
deceville <iamdecebel@gmail.com>
2018-03-08 00:12:22 +0800

    retrieving of collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,33 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
+interface Contact {
+  firstName: string;
+  secondName: string;
+  phoneNum: string;
+  mobileNum: string;
+  email: string;
+  homeAddress: string;
+  streetName: string;
+  city: string;
+  province: string;
+  country: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  contactsCol: AngularFirestoreCollection<Contact>;
+  contacts: Observable<Contact[]>;
+
   constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
-
+    this.contactsCol = this.afs.collection('contacts');
+    this.contacts = this.contactsCol.valueChanges();
   }
 }
